53cc9de01e7706aa33e53a38189533193dc98efd
Campbell Dunn <cndunn@wisc.edu>
2022-03-25 14:43:27 -0500

    Updated tests

--- tests/tests.py
+++ tests/tests.py
@@ -5,5 +5,16 @@ import pydephier
 class PyDepHierTests(unittest.TestCase):
   def test_depression_defaults(self) -> None:
     d = pydephier.Depression()
-    self.assertEqual(d.out_cell, 4294967295)
-    self.assertEqual(d.pit_cell, 4294967295)
+    self.assertEqual(d.out_cell, pydephier.NO_VALUE)
+    self.assertEqual(d.pit_cell, pydephier.NO_VALUE)
+    self.assertEqual(d.parent, pydephier.NO_PARENT)
+    self.assertEqual(d.odep, pydephier.NO_VALUE)
+    self.assertEqual(d.geolink, pydephier.NO_VALUE)
+    self.assertEqual(d.lchild, pydephier.NO_VALUE)
+    self.assertEqual(d.rchild, pydephier.NO_VALUE)
+    self.assertEqual(d.ocean_parent, False)
+    self.assertEqual(d.dep_label, 0)
+    self.assertEqual(d.cell_count, 0)
+    self.assertEqual(d.dep_vol, 0)
+    self.assertEqual(d.water_vol, 0)
+    self.assertEqual(d.total_elevation, 0)
